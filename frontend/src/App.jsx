13548bd3f90877f57a1dd4921267bc452100a1da
abhishekumar09 <2003abhishek.09@gmail.com>
2024-07-25 22:06:13 +0530

    navbar

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+// import './App.css'
+
+
+const App = () => {
+ return(
+  <div>
+   <Navbar></Navbar>
+  </div>
+ )
+}
+
+export default App
